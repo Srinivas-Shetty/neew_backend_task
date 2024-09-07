95758b634f2d1ddc96cfccda0d2a1fdcd2cb3258
Srinivas-Shetty <160107462+Srinivas-Shetty@users.noreply.github.com>
2024-09-07 23:47:19 +0530

    Update mailService.js

--- middleware/mailService.js
+++ middleware/mailService.js
@@ -10,58 +10,7 @@ function otpNDigit(otpLength){
 }
 let otp=otpNDigit(6);
 
-// let html = `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Document</title>
-// </head>
-// <body>
-//     <div style="background-color: #eceff7; margin: 30px 50px;">
-//     <table width="100%" cellpadding="0" cellspacing="0" border="0" style="margin: 0 auto; max-width: 600px;padding-top:20px;">
-//         <tr>
-//             <td style="padding: 10px 0;">
-//                 <table width="100%" cellpadding="0" cellspacing="0" border="0">
-//                     <tr>
-//                         <td align="left" style="width: 70px;">
-//                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQolS4VlcX3usdQuG4P78ITvFR3mfVvB48C0g&s" alt="logo" style="width: 70px; height: 70px;border-radius:50%;">
-//                         </td>
-//                         <td align="right">
-//                             <a href="#"><img src="https://ci3.googleusercontent.com/meips/ADKq_NZQ5f9YvhAL1WKj4Wsu4L1lnlGZ8Q7DaFF-HA99TvCUDPV092Nf89VLfNygz0IQzJD2MDLEHdTtv_PJm9ojOEie59r_tlv_vxdbmykkpl7-gk7ipQSm91KvJSPeM9iaTmI=s0-d-e1-ft#https://cdn.cutshort.io/public/images/social-media-icons/email_linkedin.png" alt="" style="width: 30px; height: 30px;"></a>
-//                             <a href="#"><img src="https://ci3.googleusercontent.com/meips/ADKq_NaSOtUV5ttC-4icXD5aLOVBf3EYBm90oEqemOcW8Lwf9MpWbn3-GODhK5qCPX-fFCzzsf8poAXFr0-B3804rHVEY47VxDjTK47MIT9qrR5rzrAkSdvwkVjTN9I1rkk68Q=s0-d-e1-ft#https://cdn.cutshort.io/public/images/social-media-icons/email_twitter.png" alt="" style="width: 30px; height: 30px;"></a>
-//                             <a href="#"><img src="https://ci3.googleusercontent.com/meips/ADKq_NY57ge-8_4KyAJ4LaM7sXNii1CrKT-KI_SR_Av4K1xJonCfCXpCwTZXvS69xe6vi1L8ziQVqXoR4vPGM5tdgkzHSFFAr9803Dk8_Ka_fWGzr0flOpEwuNEZ2sNAmMez0SeM=s0-d-e1-ft#https://cdn.cutshort.io/public/images/social-media-icons/email_instagram.png" alt="" style="width: 30px; height: 30px;"></a>
-//                         </td>
-//                     </tr>
-//                 </table>
-//             </td>
-//         </tr>
-//     </table>
-       
-
-//         <div style="background-color: white; margin: 20px 50px;">
-//             <div style="padding: 10px 30px;">
-//                 <h3>Confirm One-Time Password</h3>
-//                 <p>Hi, User</p>
-//                 <p>Please enter the following code on the page where you sent this OTP:</p>
-//                 <div style="background-color: #eceff7;">
-//                     <div style="color: blue; font-size: 35px; font-weight: 700; text-align: center; padding: 30px 0;">
-//                         ${otp}
-//                     </div>
-//                 </div>
-//                 <p>This OTP will only be valid for the next 10 minutes.</p>
-//             </div>
-//         </div>
-
-//         <div style="padding: 20px 0; text-align: center;">
-//             <p>Questions or help? <a href="mailto:srinivasyr2000@gmail.com">Email Us</a></p>
-//             <p>TMS ©2024 Implemented By ITTSTAR.COM</p>
-//         </div>
-//     </div>
-// </body>
-// </html>
-// `;
+/
 let html = `
 <!DOCTYPE html>
 <html lang="en">
